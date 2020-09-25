e5f079804c0a4a14747674eadcb59d800c291fcb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-25 12:30:21 -0400

    initial code

--- src/views/Current.svelte
+++ src/views/Current.svelte
@@ -1,20 +1,16 @@
-<nav>
-  <div>
-    <a href="">F</a>
-    |
-    <a href="">C</a>
-  </div>
-  <a href="">Favorites</a>
-</nav>
-<main>
-  <figure>
-    <img alt="partly cloudy" src="/icons/a01d.png" />
-  </figure>
-  <h3>Charleston, SC</h3>
-  <h1>74 &deg; F</h1>
-  <p>Partly Cloudy</p>
-</main>
+<script>
+  const weather = {
+    city: "Boston, MA",
+    temp: "80 &deg; F",
+    icon: "a01n",
+    description: "raining"
+  };
+</script>
+
 <style>
+  h3 {
+    color: rebeccapurple;
+  }
   nav {
     margin-bottom: 0;
     margin-left: 8px;
@@ -25,8 +21,23 @@
     flex-direction: column;
     align-items: center;
   }
-
-
 </style>
 
-
+<nav>
+  <div>
+    <a href="">F</a>
+    |
+    <a href="">C</a>
+  </div>
+  <a href="">Favorites</a>
+</nav>
+<main>
+  <figure>
+    <img alt={weather.description} src="/icons/{weather.icon}.png" />
+  </figure>
+  <h3>{weather.city}</h3>
+  <h1>
+    {@html weather.temp}
+  </h1>
+  <p>{weather.description}</p>
+</main>
